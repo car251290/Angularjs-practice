a51cc7dd61d2a22d75a5166482919400d13212e8
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-06-28 21:02:41 -0500

    Add files via upload

--- controllers.js
+++ controllers.js
@@ -1,2 +1,31 @@
 
+var myController = angular.module('myContollers',[]);
 
+myControllers.controller('SearchController',function MyController($scope,$http){
+  $http.get('js/data.json').then(function(response){
+    //get the data 
+    $scope.artists = response.data;
+    $scope.artistOrder = 'name';
+  });
+});
+
+myControllers.controller('DetailsController',function MyController($scope,$http,$routeParams){
+    $http.get('js/data.json').then(function(response){
+      //get the data 
+      $scope.artists = response.data;
+      $scope.whichItem = $routeParams.itemId;
+
+      if($routeParams.itemId > 0){
+          $scope.prevItem = Number($routeParams.itemId) -1;
+      } else {
+          $scope.prevItem = $scope.artistOrder.length-1;
+
+      }
+      if($routeParams.itemId < $scope.artistOrder.length-1) {
+          $scope.nextItem = Number($routerParams.itemId) + 1;
+
+      } else {
+          $scope.nextItem =  0;
+      }
+    });
+  });
